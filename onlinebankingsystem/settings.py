9ed475126985d1596d3247331a5470e78774c3c2
Pritam Shaw <thepritamshaw@gmail.com>
2024-03-26 11:14:06 +0530

    allowed hosts

--- onlinebankingsystem/settings.py
+++ onlinebankingsystem/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--+^-cj&-)p&*34j*5al54d_-ffj^@deeax!1mh1r_538gfwn()'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
